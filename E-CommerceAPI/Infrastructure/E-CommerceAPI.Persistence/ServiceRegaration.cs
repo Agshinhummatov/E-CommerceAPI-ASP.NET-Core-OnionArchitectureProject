c7080c4b7aceb8a6488a14a4c43eef1e9ee2e408
Agshinhummatov <agshinshh@code.edu.az>
2023-12-04 13:28:37 +0400

    Updated Repo

--- E-CommerceAPI/Infrastructure/E-CommerceAPI.Persistence/ServiceRegaration.cs
+++ E-CommerceAPI/Infrastructure/E-CommerceAPI.Persistence/ServiceRegaration.cs
@@ -1,4 +1,6 @@
-﻿using E_CommerceAPI.Persistence.Contexts;
+﻿using E_CommerceAPI.Application.Repositories;
+using E_CommerceAPI.Persistence.Contexts;
+using E_CommerceAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +12,17 @@ namespace E_CommerceAPI.Persistence
         public static void AddPresistenceServices(this IServiceCollection services)
         {
 
-            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseSqlServer(Configuration.ConnectionString) ,ServiceLifetime.Singleton);
+
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+
+
+
         }
 
     }
